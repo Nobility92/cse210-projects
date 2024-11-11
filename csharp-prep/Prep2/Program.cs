5c7d4d7724a274cd85d672310eff957b819ed595
Nobility92 <nwa21004@byui.edu>
2024-11-11 22:14:57 +0100

    prep

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        char letter;
+
+        Console.WriteLine("What is your Grade Percentage?");
+        int grade = Console.ReadLine();
+        
+        if (grade >= 90)
+        {
+            letter = 'A';
+            //Console.WriteLine("Your grade is A");
+        }
+        else if (grade >= 80)
+        {
+            letter = 'B';
+            //Console.WriteLine("Your grade is B");
+        }
+         else if (grade >= 70)
+        {
+            letter = 'C';
+            //Console.WriteLine("Your grade is C");
+        }
+         else if (grade >= 60)
+        {
+            letter = 'D';
+            //Console.WriteLine("Your grade is D");
+        }
+        else
+        letter = 'F';
+         //Console.WriteLine("You Failed");
+         Console.WriteLine($"Your grade is {letter}");
     }
 }
