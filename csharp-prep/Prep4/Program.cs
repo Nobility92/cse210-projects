fdd486fd0aa3e3e28869f5087b53a40ce6d81fe5
Nobility92 <nwa21004@byui.edu>
2024-11-12 20:07:00 +0100

    prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        
+        int Number = -1;
+        while (Number != 0)
+        {
+            Console.Write("Enter a number (type 0 to stop): ");
+            string answer = Console.ReadLine();
+            Number = int.Parse(answer);
+            
+            // adding the number to the list
+            if (Number != 0)
+            {
+                numbers.Add(Number);
+            }
+        }
+
+        //Compute the sum
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        // Compute the average
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Find the max
+        
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
