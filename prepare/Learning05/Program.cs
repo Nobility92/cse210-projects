bc7458b3a70056fa3441771f7a845f7a489bb76d
Nobility92 <nwa21004@byui.edu>
2024-12-10 14:41:47 +0100

    Assignment

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment assignment1 = new Assignment("Henry Nwaokonko", "Inheritance");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment mathAssignment1 = new MathAssignment("Wendy Kalu", "Abstraction", "10.5", "5-9");
+        Console.WriteLine(mathAssignment1.GetSummary());
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+        Console.WriteLine();
+        
+        WritingAssignment writingAssignment = new WritingAssignment("David Nwaokonko", "Prose", "Things Fall Apart");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
