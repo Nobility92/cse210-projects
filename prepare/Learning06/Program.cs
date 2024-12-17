8d80cec0e8e8797569908c515a8d34027dd9d21e
Nobility92 <nwa21004@byui.edu>
2024-12-17 11:41:22 +0100

    shapes

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square squ = new Square("Blue", 3.2);
+        shapes.Add(squ);
+
+        Rectangle rec = new Rectangle(6, 4.5, " Yellow");
+        shapes.Add(rec);
+
+        Circle cir = new Circle("Red", 5);
+        shapes.Add(cir);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"Color: {color }, Area: { area}");
+        } 
     }
 }
